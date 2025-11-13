a601a383c78dfe74b0892873a7024caf051c2710
David Johnson <david_karl_johnson@hotmail.com>
2025-11-13 19:12:15 +0100

    Complete lab1, task3

--- lab1/task3/authentication.py
+++ lab1/task3/authentication.py
@@ -5,7 +5,6 @@ import math
 script_dir = os.path.dirname(os.path.abspath(__file__))
 words_path = os.path.join(script_dir, 'wordslist.txt')
 
-
 def file_to_list(path):
     with open(path, 'r', encoding='utf-8') as fh:
         return fh.read().splitlines()
@@ -14,7 +13,7 @@ def file_to_list(path):
 def build_password(nr_words, words_list):
     password = ''
     for i in range (nr_words):
-        password += words_list[random.randrange(0, len(words_list))]
+        password += words_list[random.randrange(0, len(words_list) - 1)]
     return password
 
 
@@ -23,9 +22,13 @@ def calculate_entropy(str):
     return math.log2(62) * len(str)
 
 
-
 words_list = file_to_list(words_path)
 
-print(build_password(2, words_list))
-
-
+while True:
+    nr_words = int(input('Number of words: '))
+    pw = build_password(nr_words, words_list)
+    entropy = calculate_entropy(pw)
+    print(pw)
+    print(f'{nr_words} RANDOM words from word list of {(len(words_list))} words')
+    print(f'Entropy: {entropy} bits')
+    print(f'You need {entropy / calculate_entropy('a')} RANDOM characters in [a-z, A-Z, 0-9] to get the same entropy.\n')
