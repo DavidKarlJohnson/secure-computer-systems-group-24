a83f2699d1d5cb659659032e31ac1961862405af
David Johnson <david_karl_johnson@hotmail.com>
2025-11-13 18:56:45 +0100

    Create function for calculating entropy

--- lab1/task3/authentication.py
+++ lab1/task3/authentication.py
@@ -1,5 +1,6 @@
 import os
 import random
+import math
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 words_path = os.path.join(script_dir, 'wordslist.txt')
@@ -13,8 +14,13 @@ def file_to_list(path):
 def build_password(nr_words, words_list):
     password = ''
     for i in range (nr_words):
-        password += " " + words_list[random.randrange(0, len(words_list))]
+        password += words_list[random.randrange(0, len(words_list))]
     return password
+
+
+# a-z, A-Z, 0-9
+def calculate_entropy(str):
+    return math.log2(62) * len(str)
 
 
 
